21fc29ec9299c308eba2bdf464c40f358eba6e97
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-03-03 20:39:10 +1100

    Format and udpate database schema

--- PHASE_1/API_SourceCode/server.js
+++ PHASE_1/API_SourceCode/server.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 // Require process, so we can mock environment variables.
-const process = require('process');
-const express = require('express');
-const db = require('./database');
+const process = require("process");
+const express = require("express");
+const db = require("./database");
 
 const app = express();
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 // Automatically parse request body as form data.
 app.use(express.urlencoded({ extended: false }));
@@ -15,16 +15,16 @@ app.use(express.json());
 
 // Set Content-Type for all responses for these routes.
 app.use((req, res, next) => {
-    res.set('Content-Type', 'application/json');
+    res.set("Content-Type", "application/json");
     next();
 });
 
 // Create a Winston logger that streams to Stackdriver Logging.
-const winston = require('winston');
-const { LoggingWinston } = require('@google-cloud/logging-winston');
+const winston = require("winston");
+const { LoggingWinston } = require("@google-cloud/logging-winston");
 const loggingWinston = new LoggingWinston();
 const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     transports: [new winston.transports.Console(), loggingWinston],
 });
 
@@ -43,40 +43,50 @@ app.use(async (req, res, next) => {
     }
 });
 
-app.get('/articles', async (req, res) => {
+app.get("/articles", async (req, res) => {
     _conn = _conn || (await db.createConnectionPool());
 
-    const articles = await _conn.select('*').from('article');
+    const articles = await _conn.select("*").from("article");
 
     res.send(articles);
 });
 
-app.get('/admin/reset', async (req, res) => {
+app.get("/admin/reset", async (req, res) => {
     _conn = _conn || (await db.createConnectionPool());
 
     try {
-        await _conn.schema.dropTableIfExists('article');
-        await _conn.schema.createTable('article', table => {
-            table.string('url')             .primary();
-            table.string('title')           .notNullable();
-            table.string('author')          .notNullable();
-            table.date  ("publication_date").notNullable();
-            table.string('body')            .notNullable();
-            table.string('hash')            .notNullable();
+        await _conn.schema.dropTableIfExists("article");
+        await _conn.schema.createTable("article", (table) => {
+            table.string("url").primary();
+            table.string("headline").notNullable();
+            table.string("main_text").notNullable();
+            table.string("category");
+            table.string("author").notNullable();
+            table.string("date_of_publication").notNullable();
         });
 
+        await _conn.schema.dropTableIfExists("report");
+        await _conn.schema.createTable("report", (table) => {
+            table.string("id").primary();
+            table.date("event_date").notNullable();
+            table.string("syndrome").notNullable();
+            table.string("disease").notNullable();
+            table.string("source");
+            table.string("url").references("url").inTable("article");
+            table.string("country").notNullable();
+            table.string("city");
+        });
         res.send({ status: "success" });
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
         res.send(error);
     }
-})
+});
 
 const PORT = parseInt(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+    console.log("Press Ctrl+C to quit.");
 });
 
 module.exports = server;
