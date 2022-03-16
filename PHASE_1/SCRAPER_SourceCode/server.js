8dda05f39b81ddd7e3f3599779d01d58a1cf9d6d
alexandermbrown <alexbr5@bigpond.com>
2022-03-16 12:21:10 +1100

    Update scraper to be run by using crontab

--- PHASE_1/SCRAPER_SourceCode/server.js
+++ PHASE_1/SCRAPER_SourceCode/server.js
@@ -4,17 +4,32 @@
 const db = require("./database");
 const scraper = require("./scraper");
 
-let _conn;
+async function scrape() {
+    let conn;
 
-exports.scrape = async (message, context) => {
-    _conn = _conn || await db.createConnectionPool();
+    try {
+        conn = conn || await db.createConnectionPool();
+    }
+    catch (error) {
+        console.log("Failed to connect to database.");
+        console.log(error);
+        return;
+    }
 
     console.log("Scraping...");
 
     const articles = [];
-    await scraper.scrape(
-        article => articles.push(article)
-    );
+    try {
+        await scraper.scrape(
+            article => articles.push(article)
+        );
 
-    console.log("Finished.");
-};
+        console.log("Success.");
+    }
+    catch (error) {
+        console.log("Failed to scrape. ");
+        console.log(error);
+    }
+}
+
+scrape();
