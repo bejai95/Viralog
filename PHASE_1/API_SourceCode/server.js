30d4ad04e98a9306baaaf5fea7366be295aeccad
alexandermbrown <alexbr5@bigpond.com>
2022-03-18 21:38:44 +1100

    Improve error handling

--- PHASE_1/API_SourceCode/server.js
+++ PHASE_1/API_SourceCode/server.js
@@ -27,25 +27,14 @@ app.get("/articles", async (req, res) => {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
     if (!req.query) {
-        const message = "Missing query parameters";
-        createLog(_conn, ip, "/articles", req.query, 400, message);
-        return res.status(400).send({ status: 400, message: message });
+        return performError(res, "/articles", 400, "Missing query parameters", req.query, ip);
     }
-    if (!req.query.period_of_interest_start) {
-        const message = "Missing parameter period_of_interest_start";
-        createLog(_conn, ip, "/articles", req.query, 400, message);
-        return res.status(400).send({ status: 400, message: message });
+    const nullValue = findNull(req.query, ["period_of_interest_start", "period_of_interest_end", "key_terms", "location"]);
+    if (nullValue) {
+        return performError(res, "/articles", 400, `Missing parameter ${nullValue}`, req.query, ip);
     }
-    if (!req.query.period_of_interest_end) {
-        const message = "Missing parameter period_of_interest_end";
-        createLog(_conn, ip, "/articles", req.query, 400, message);
-        return res.status(400).send({ status: 400, message: message });
-    }
-    
     if (typeof req.query.key_terms !== "string") {
-        const message = "key_terms must be a string";
-        createLog(_conn, ip, "/articles", req.query, 400, message);
-        return res.status(400).send({ status: 400, message: message });
+        return performError(res, "/articles", 400, "key_terms must be a string", req.query, ip);
     }
 
     // /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/
@@ -63,7 +52,7 @@ app.get("/articles", async (req, res) => {
     }
     catch (error) {
         console.log(error);
-        res.status(500).send({ status: 500, message: "An internal server error occurred. " + error });
+        return performError(res, "/articles", 500, "An internal server error occurred. " + error, req.query, ip);
     }
 });
 
@@ -71,26 +60,19 @@ app.get("/reports", async (req, res) => {
     _conn = _conn || (await db.createConnectionPool());
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    if (!req.query)
+    if (!req.query) {
         return res.status(400).send({ status: 400, message: "Missing query parameters" });
-    if (!req.query.period_of_interest_start) {
-        return res
-            .status(400)
-            .send({ status: 400, message: "Missing parameter period_of_interest_start" });
     }
-    if (!req.query.period_of_interest_end) {
-        return res
-            .status(400)
-            .send({ status: 400, message: "Missing parameter period_of_interest_end" });
+    const nullValue = findNull(req.query, ["period_of_interest_start", "period_of_interest_end", "key_terms", "location"]);
+    if (nullValue) {
+        return performError(res, "/reports", 400, `Missing parameter ${nullValue}`, req.query, ip);
     }
-    
+
     if (typeof req.query.key_terms !== "string") {
-        return res.status(400).send({ status: 400, message: "key_terms must be a string" });
+        return performError(res, "/reports", 400, "key_terms must be a string", req.query, ip);
     }
     if (typeof req.query.location !== "string") {
-        const message = "location must be a string";
-        createLog(_conn, ip, "/reports", req.query, 400, message, req.query.team);
-        return res.status(400).send({ status: 400, message: message });
+        return performError(res, "/reports", 400, "location must be a string", req.query, ip);
     }
 
     try {
@@ -101,11 +83,11 @@ app.get("/reports", async (req, res) => {
             req.query.location,
         );
         createLog(_conn, ip, "/reports", req.query, 200, "success", req.query.team);
-        res.send(results);
+        return res.send(results);
     }
     catch (error) {
         console.log(error);
-        res.status(500).send({ status: 500, message: "An internal server error occurred. " + error });
+        return performError(res, "/reports", 500, "An internal server error occurred. " + error, req.query, ip);
     }
 });
 
@@ -120,13 +102,22 @@ app.get("/predictions", async (req, res) => {
     }
     catch(error) {
         console.log(error);
-        res.status(500).send({ status: 500, message: "An internal server error occurred. " + error });
+        return performError(res, "/predictions", 500, "An internal server error occurred. " + error, req.query, ip);
     }
 });
 
 app.get("/logs", async (req, res) => {
     _conn = _conn || (await db.createConnectionPool());
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    if (!req.query) {
+        return performError(res, "/logs", 400, "Missing query parameters", req.query, ip);
+    }
+    
+    if (req.query.routes && typeof req.query.routes !== "string") {
+        return performError(res, "/logs", 400, "routes must be a string", req.query, ip);
+    }
+    // const timeFormat = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/;
 
     try {
         const logs = await routes.logs(_conn, ip);
@@ -139,18 +130,33 @@ app.get("/logs", async (req, res) => {
     }
 });
 
-async function createLog(conn, ip, route, reqParams, status, message, team) {
+function findNull(obj, keys) {
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        if (!(key in obj) || obj[key] == null) {
+            return key;
+        }
+    }
+    return null;
+}
+
+function performError(res, route, status, message, query, ip) {
+    createLog(_conn, ip, route, query, 400, message, query.team);
+    return res.status(400).send({ status: 400, message: message });
+}
+
+async function createLog(conn, ip, route, queryParams, status, message) {
     let timestamp = (new Date().toISOString());
     timestamp = timestamp.replace(/\.[0-9]{3}Z$/, "");
 
     await conn("Log").insert({
         status: status,
         route: route,
-        req_params: JSON.stringify(reqParams),
+        req_params: JSON.stringify(queryParams),
         timestamp: timestamp,
         message: message,
         ip: ip,
-        team: team || "Team QQ"
+        team: queryParams.team || "Team QQ"
     });
 }
 
