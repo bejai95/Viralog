c15ced9c8c67409a03988bc9a8eafc77e92fe5ae
Dan Shea <danshea00@gmail.com>
2022-03-20 13:01:37 +1100

    test framework

--- PHASE_1/API_SourceCode/babel.config.js
+++ PHASE_1/API_SourceCode/babel.config.js
@@ -1,1 +1,13 @@
-module.exports = {presets: ['@babel/preset-env']}
+// babel.config.js
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+  };
