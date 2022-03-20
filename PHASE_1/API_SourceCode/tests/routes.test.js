9b2edf5e64001ad02505972f1a0a1e55ff5965f0
Dan Shea <danshea00@gmail.com>
2022-03-21 09:31:39 +1100

    Tests working

--- PHASE_1/API_SourceCode/tests/routes.test.js
+++ PHASE_1/API_SourceCode/tests/routes.test.js
@@ -15,8 +15,8 @@ describe("Reports Endpoint", () => {
         .get("/reports")
         .set("Content-Type", "application/json")
         .query({
-            period_of_interest_start: "2022-03-13T12:21:21",
-            period_of_interest_end: "2022-03-13T17:21:21",
+            period_of_interest_start: "2022-03-13T13:00:00",
+            period_of_interest_end: "2022-03-13T13:00:01",
             key_terms: "COVID-19",
             location: "China",
         })
@@ -35,12 +35,30 @@ describe("Reports Endpoint", () => {
         .get("/reports")
         .set("Content-Type", "application/json")
         .query({
-            period_of_interest_start: "2020-03-13T12:21:21",
-            period_of_interest_end: "2022-03-13T17:21:21",
+            period_of_interest_start: "2017-03-13T12:21:21",
+            period_of_interest_end: "2017-03-13T12:21:22",
             key_terms: "COVID-18",
             location: "China",
         })
         .expect(200)
+        .end((err, res) => {
+            expect(res.body).toEqual([]);
+            done();
+        });
+    });
+
+    test("Date in wrong order", (done) => {
+        request(app)
+        .get("/reports")
+        .set("Content-Type", "application/json")
+        .query({
+            period_of_interest_start: "2022-03-13T12:21:21",
+            period_of_interest_end: "2020-02-13T12:21:21",
+            key_terms: "COVID-19",
+            location: "China",
+        })
+        .expect(400)
+        .send()
         .end((err, res) => {
             expect(res.body).toEqual([]);
             done();
@@ -52,16 +70,15 @@ describe("Reports Endpoint", () => {
         .get("/reports")
         .set("Content-Type", "application/json")
         .query({
-            period_of_interest_start: "2023-03-13T12:21:21",
-            period_of_interest_end: "2022-03-13T17:21:21",
+            period_of_interest_start: "2020-03-13T12:21:21",
+            period_of_interest_end: "2022-03-13X17:21:21",
             key_terms: "COVID-19",
             location: "China",
         })
         .expect(400)
         .send()
         .end((err, res) => {
-            expect(res.body["message"]).toEqual("Invalid timestamp for 'period_of_interest_end', must be after 'period_of_interest_start'");
-            expect(res.body["status"]).toEqual(400);
+            expect(res.body['message']).toEqual("Invalid timestamp for 'period_of_interest_end', must be in format 'yyyy-MM-ddTHH:mm:ss'");
             done();
         });
     });
@@ -74,7 +91,7 @@ describe("Articles Endpoint", () => {
         .set("Content-Type", "application/json")
         .query({
             period_of_interest_start: "2022-03-13T12:21:21",
-            period_of_interest_end: "2022-03-13T17:21:21",
+            period_of_interest_end: "2022-03-13T13:09:06",
             key_terms: "COVID-19",
             location: "China",
         })
@@ -111,8 +128,8 @@ describe("Predictions Endpoint", () => {
         .get("/predictions")
         .set("Content-Type", "application/json")
         .query({
-            min_reported_cases: 8,
-            day_count: 4,
+            min_report_count: 1,
+            day_count: 5,
         })
         .expect(200)
         .end((err, res) => {
@@ -138,7 +155,8 @@ const routes_test_expected = [
           "vomiting",
           "loss of taste",
           "loss of smell"
-       ]
+       ],
+       "url": "https://www.cidrap.umn.edu/news-perspective/2022/03/chinas-omicron-covid-19-surge-gains-steam",
     }
 ]
 
@@ -166,7 +184,10 @@ const articles_test_expected = [
           "event_date": "2022-03-13T13:00:00",
           "location": "China"
         }
-      ]
+      ],
+      "category": "COVID-19",
+      "author": "Lisa Schnirring | News Editor | CIDRAP News",
+      "source": "CIDRAP"
     }
   ]
 
@@ -218,235 +239,11 @@ const predictions_test_expected = [
       "disease": "COVID-19",
       "reports": [
         {
-          "report_id": 24,
-          "event_date": "2022-03-06T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/experts-map-out-new-normal-us-enters-third-pandemic-year",
-          "location": "America"
-        },
-        {
-          "report_id": 32,
-          "event_date": "2022-03-01T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/test-treat-variant-vaccines-part-new-federal-covid-19-plan",
-          "location": "America"
-        },
-        {
-          "report_id": 20,
-          "event_date": "2022-03-07T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/who-lays-out-plan-covid-vaccines-tackle-new-variants",
-          "location": "Asia—"
-        },
-        {
-          "report_id": 5,
-          "event_date": "2022-03-15T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/twice-many-black-covid-patients-deemed-lowest-priority-icu-triage-system",
-          "location": "Boston"
-        },
-        {
-          "report_id": 10,
-          "event_date": "2022-03-14T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/study-highlights-risk-low-covid-vaccine-uptake-prison-staff",
-          "location": "California"
-        },
-        {
-          "report_id": 12,
-          "event_date": "2022-03-10T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/chinese-provincial-capital-locks-down-countrys-covid-cases-rise",
-          "location": "China"
-        },
-        {
-          "report_id": 9,
-          "event_date": "2022-03-13T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/chinas-omicron-covid-19-surge-gains-steam",
-          "location": "China"
-        },
-        {
-          "report_id": 11,
-          "event_date": "2022-03-14T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/chinas-covid-19-cases-double-other-nations-eye-resurgences",
-          "location": "China"
-        },
-        {
-          "report_id": 27,
-          "event_date": "2022-03-06T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/health-groups-press-more-wildlife-sars-cov-2-tracking",
-          "location": "China"
-        },
-        {
-          "report_id": 41,
-          "event_date": "2022-02-22T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/global-covid-19-cases-fall-except-asian-hot-spots",
-          "location": "Egypt Kenya Nigeria Senegal South Africa"
-        },
-        {
-          "report_id": 22,
-          "event_date": "2022-03-02T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/third-vaccine-dose-boosts-omicron-protection-some-waning",
-          "location": "England"
-        },
-        {
-          "report_id": 38,
-          "event_date": "2022-02-24T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/cdc-eases-covid-19-mask-guidance-adds-metrics-future-use",
-          "location": "Hawaii"
-        },
-        {
-          "report_id": 43,
-          "event_date": "2022-02-21T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/hong-kong-harness-mass-testing-omicron-battle",
-          "location": "Hong Kong"
-        },
-        {
-          "report_id": 4,
-          "event_date": "2022-03-15T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/severe-covid-19-tied-long-term-depression-anxiety",
-          "location": "Iceland"
-        },
-        {
-          "report_id": 31,
-          "event_date": "2022-03-01T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/survival-after-hospital-cardiac-arrest-35-lower-covid-19-patients",
-          "location": "Iowa"
-        },
-        {
-          "report_id": 37,
-          "event_date": "2022-02-24T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/studies-no-very-slight-risk-hearing-loss-after-covid-vaccine",
-          "location": "Israel"
-        },
-        {
-          "report_id": 29,
-          "event_date": "2022-03-03T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/la-drops-mask-mandate-nfl-drops-covid-19-protocols",
-          "location": "Los Angeles County"
-        },
-        {
-          "report_id": 15,
-          "event_date": "2022-03-09T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/study-third-covid-mrna-vaccine-dose-needed-against-omicron",
-          "location": "Michigan"
-        },
-        {
-          "report_id": 25,
-          "event_date": "2022-03-03T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/covid-only-minnesota-hospitals-had-lower-death-rates",
-          "location": "Minnesota"
-        },
-        {
-          "report_id": 33,
-          "event_date": "2022-02-28T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/polls-show-americans-less-worried-about-covid-19",
-          "location": "New York"
-        },
-        {
-          "report_id": 36,
-          "event_date": "2022-02-27T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/more-mask-mandates-fall-poor-covid-vaccine-protection-noted-young-kids",
-          "location": "New York"
-        },
-        {
-          "report_id": 26,
-          "event_date": "2022-03-02T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/asias-omicron-surges-soar-us-adds-technology-who-led-push",
-          "location": "New Zealand"
-        },
-        {
-          "report_id": 34,
-          "event_date": "2022-03-01T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/global-covid-cases-deaths-drop-except-key-hot-spots",
-          "location": "New Zealand"
-        },
-        {
-          "report_id": 8,
-          "event_date": "2022-03-10T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/kids-asthma-not-higher-risk-covid-19-study-finds",
-          "location": "North Carolina"
-        },
-        {
-          "report_id": 35,
-          "event_date": "2022-02-28T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/dementia-patients-died-higher-rates-during-pandemic",
-          "location": "Philadelphia"
-        },
-        {
-          "report_id": 42,
-          "event_date": "2022-02-23T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/us-officials-plan-next-pandemic-phase-vaccine-uptake-drops-globally",
-          "location": "Russia"
-        },
-        {
-          "report_id": 45,
-          "event_date": "2022-02-21T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/3-covid-vaccine-doses-99-effective-against-omicron-delta-hospitalization",
-          "location": "Southern California"
-        },
-        {
           "report_id": 6,
           "event_date": "2022-03-16T13:00:00",
           "disease_id": "COVID-19",
           "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/covid-19-cases-soar-asian-hot-spots-us-gets-new-covid-czar",
           "location": "South Korea"
-        },
-        {
-          "report_id": 7,
-          "event_date": "2022-03-15T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/global-covid-cases-rising-again",
-          "location": "South Korea Vietnam Germany"
-        },
-        {
-          "report_id": 17,
-          "event_date": "2022-03-10T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/global-covid-19-deaths-may-be-3-times-higher-recorded",
-          "location": "Sweden"
-        },
-        {
-          "report_id": 30,
-          "event_date": "2022-02-27T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/medical-oxygen-supplies-running-out-besieged-ukraine",
-          "location": "Ukraine"
-        },
-        {
-          "report_id": 40,
-          "event_date": "2022-02-23T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/covid-19-pregnancy-tied-poor-birth-outcomes",
-          "location": "United Kingdom"
-        },
-        {
-          "report_id": 18,
-          "event_date": "2022-03-09T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/covid-19-remains-high-uk-ba2-gains-ground",
-          "location": "United Kingdom"
         },
         {
           "report_id": 1,
@@ -456,95 +253,13 @@ const predictions_test_expected = [
           "location": "United Kingdom Ireland"
         },
         {
-          "report_id": 19,
-          "event_date": "2022-03-08T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/global-drop-covid-19-continues-cases-still-high",
-          "location": "United States"
-        },
-        {
           "report_id": 3,
           "event_date": "2022-03-17T13:00:00",
           "disease_id": "COVID-19",
           "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/studies-show-vaccines-protect-against-serious-omicron-illness",
           "location": "United States"
-        },
-        {
-          "report_id": 39,
-          "event_date": "2022-02-22T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/mis-c-rare-covid-vaccinated-teens-study-finds",
-          "location": "United States"
-        },
-        {
-          "report_id": 44,
-          "event_date": "2022-02-20T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/02/uk-unveils-game-plan-living-covid",
-          "location": "United States"
-        },
-        {
-          "report_id": 28,
-          "event_date": "2022-03-02T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/rural-urban-disparities-seen-us-covid-19-vaccine-uptake",
-          "location": "United States"
-        },
-        {
-          "report_id": 21,
-          "event_date": "2022-03-07T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/most-mrna-covid-vaccine-adverse-events-mild-transient",
-          "location": "United States"
-        },
-        {
-          "report_id": 23,
-          "event_date": "2022-03-06T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/covid-deaths-vary-race-community-social-factors",
-          "location": "Washington"
-        },
-        {
-          "report_id": 16,
-          "event_date": "2022-03-08T13:00:00",
-          "disease_id": "COVID-19",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/mental-decline-seen-older-covid-patients-1-year-later",
-          "location": "Wuhan China"
         }
       ],
-      "threshold": 0.5833333333333334
-    },
-    {
-      "disease": "polio",
-      "reports": [
-        {
-          "report_id": 2,
-          "event_date": "2022-03-17T13:00:00",
-          "disease_id": "polio",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/new-polio-cases-africa-malawi-launches-vaccine-campaign",
-          "location": "Nigeria"
-        }
-      ],
-      "threshold": 0.03225806451612903
-    },
-    {
-      "disease": "tuberculosis",
-      "reports": [
-        {
-          "report_id": 13,
-          "event_date": "2022-03-14T13:00:00",
-          "disease_id": "tuberculosis",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/rsv-vaccine-could-cut-antimicrobial-use-infants",
-          "location": "California"
-        },
-        {
-          "report_id": 14,
-          "event_date": "2022-03-10T13:00:00",
-          "disease_id": "tuberculosis",
-          "article_url": "https://www.cidrap.umn.edu/news-perspective/2022/03/trial-supports-shorter-drug-regimen-kids-non-severe-tb",
-          "location": "New"
-        }
-      ],
-      "threshold": 0.0625
+      "report_count": 3
     }
   ]
