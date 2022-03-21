768bb1186e2ca472f2b45599718438bdcbfe6de2
alexandermbrown <alexbr5@bigpond.com>
2022-03-21 12:57:57 +1100

    Update TestScripts

--- PHASE_1/TestScripts/runtests.sh
+++ PHASE_1/TestScripts/runtests.sh
@@ -1,2 +1,4 @@
+# Please see API_SourceCode/tests and SCRAPER_SourceCode/article-processor.test.js for actual tests.
+
 cd ../API_SourceCode && npm install && npm test
 cd ../SCRAPER_SourceCode && npm install && npm test
