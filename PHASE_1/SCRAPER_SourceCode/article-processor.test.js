24302fd803df9c082fe87b1909823e3dfa7db80f
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-03-21 13:17:30 +1100

    Add unit tests

--- PHASE_1/SCRAPER_SourceCode/article-processor.test.js
+++ PHASE_1/SCRAPER_SourceCode/article-processor.test.js
@@ -1,7 +1,145 @@
 const db = require("./database");
-const { processArticle } = require("./article-processor");
+const { processArticle, findReports } = require("./article-processor");
 
+/****************************************/
+/*              Unit tests              */
+/****************************************/
+const testDiseases = {
+    anthrax: { anthrax: 'anthrax' },
+    'avian flu': {
+      'avian flu': 'avian flu',
+      avianflu: 'avian flu',
+      H5N1: 'avian flu',
+      'avian influenza': 'avian flu'
+    },
+    'hiv/aids': {
+      HIV: 'hiv/aids',
+      AIDS: 'hiv/aids',
+      'human immunodeficiency virus': 'hiv/aids',
+      'acquired immunodeficiency syndrome': 'hiv/aids'
+    },
+    hpv: { hpv: 'hpv', 'human papillomavirus infection': 'hpv' },
+    legionnaire: {
+      legionnaire: 'legionnaire',
+      'legionella pneumonia': 'legionnaire',
+      "Legionnaires' disease": 'legionnaire'
+    },
+  }
 
+test("test_simple_report", async () => {
+    let article = {
+        headline: "Something around the world",
+        date_of_publication: "Mar 15, 2022",
+        author: "Alex",
+        main_text: "We found a case of legionnaire in Thailand.",
+        article_url: "www.a_standard_disease_site.com",
+        category: "news"
+    }
+    let targetReport = {
+        article_url: "www.a_standard_disease_site.com",
+        disease_id: "legionnaire",
+        event_date: "Mar 15, 2022",
+        location: "Thailand"
+    }
+
+    let processed = await findReports(article, testDiseases);
+    expect(JSON.stringify(targetReport)).toBe(JSON.stringify(processed[0]))
+})
+
+test("test_using_aliases", async () => {
+    let article = {
+        headline: "Something around the world",
+        date_of_publication: "Mar 15, 2022",
+        author: "Alex",
+        main_text: "We found a case of legionella pneumonia in Thailand.",
+        article_url: "www.a_standard_disease_site.com",
+        category: "news"
+    }
+    let targetReport = {
+        article_url: "www.a_standard_disease_site.com",
+        disease_id: "legionnaire",
+        event_date: "Mar 15, 2022",
+        location: "Thailand"
+    }
+
+    let processed = await findReports(article, testDiseases);
+    expect(JSON.stringify(targetReport)).toBe(JSON.stringify(processed[0]))
+})
+
+test("test_multiple_reports", async () => {
+    let article = {
+        headline: "Something around the world",
+        date_of_publication: "Mar 15, 2022",
+        author: "Alex",
+        main_text: "We found a case of legionnaire in Thailand. Also someone may have hiv/aids in Australia",
+        article_url: "www.a_standard_disease_site.com",
+        category: "news"
+    }
+
+    let targets = {
+        Thailand: {
+            article_url: "www.a_standard_disease_site.com",
+            disease_id: "legionnaire",
+            event_date: "Mar 15, 2022",
+            location: "Thailand"
+        },
+        Australia: {
+            article_url: "www.a_standard_disease_site.com",
+            disease_id: "hiv/aids",
+            event_date: "Mar 15, 2022",
+            location: "Australia"
+        }
+    }
+    let processed = await findReports(article, testDiseases);
+    expect(processed.length).toBe(2);
+    for (let i = 0; i < processed.length; i++) {
+        expect(JSON.stringify(targets[processed[i].location])).toBe(JSON.stringify(processed[i]))
+        delete targets[processed[i].location]
+    }
+})
+
+test("test_multiple_reports_different_aliases", async () => {
+    let article = {
+        headline: "Something around the world",
+        date_of_publication: "Mar 15, 2022",
+        author: "Alex",
+        main_text: "We found a case of legionella pneumonia in Thailand. Don't forget that someone in the United States has AIDS. Sudden resurgence of human papillomavirus infection in England.",
+        article_url: "www.a_standard_disease_site.com",
+        category: "news"
+    }
+
+    let targets = {
+        Thailand: {
+            article_url: "www.a_standard_disease_site.com",
+            disease_id: "legionnaire",
+            event_date: "Mar 15, 2022",
+            location: "Thailand"
+        },
+        'United States': {
+            article_url: "www.a_standard_disease_site.com",
+            disease_id: "hiv/aids",
+            event_date: "Mar 15, 2022",
+            location: "United States"
+        },
+        England: {
+            article_url: "www.a_standard_disease_site.com",
+            disease_id: "hpv",
+            event_date: "Mar 15, 2022",
+            location: "England"
+        }
+    }
+    let processed = await findReports(article, testDiseases);
+    expect(processed.length).toBe(3);
+    for (let i = 0; i < processed.length; i++) {
+        expect(JSON.stringify(targets[processed[i].location])).toBe(JSON.stringify(processed[i]))
+        delete targets[processed[i].location]
+    }
+
+})
+
+/****************************************/
+/*           Integration tests          */
+/****************************************/
 test("test_article_invalid_article_empty", async () => {
     let conn;
     try {
