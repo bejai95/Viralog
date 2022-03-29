9774eab7bcf349a4fc6d0b3b750020cc1727f993
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2022-03-29 20:13:21 +1100

    Made a diseases page and some styling

--- PHASE_2/Application_SourceCode/pages/diseases/diseasePage.js
+++ PHASE_2/Application_SourceCode/pages/diseases/diseasePage.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useContext, useState } from "react";
 import NavBar from "../../components/NavBar";
 import styles from "../../styles/Disease.module.scss";
 
-export default function DiseasePage() {
-  const [errMsg, setError] = useState();
+export default function DiseaseInfoPage() {
   const diseaseName = "COVID-19"; // Later this will be retrieved by the API
   const paraContent = "COVID, COVID-19, Coronavirus, ..." // Later this will be retrieved by the API
 
@@ -16,18 +14,15 @@ export default function DiseasePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <div className="content_main">
-        <div className={styles.disease_content}>
-          { errMsg && <div><b style={{color: "red"}}>{errMsg}</b></div> }
-          <h1>{diseaseName}</h1>
-          <h2>Also known as</h2>
-          <p>{paraContent}</p>
-          <h2>Symptoms</h2>
-          <br></br>
-          <h2>Visualisation of case/report frequency around the world</h2>
-          <br></br>
-          <h2>Predictions (if they exist)</h2>
-        </div>
+      <div className={styles.content}>
+        <h1>{diseaseName}</h1>
+        <h2>Also known as</h2>
+        <p>{paraContent}</p>
+        <h2>Symptoms</h2>
+        <br></br>
+        <h2>Visualisation of case/report frequency around the world</h2>
+        <br></br>
+        <h2>Predictions (if they exist)</h2>
       </div>
     </>
   );
---
+++ PHASE_2/Application_SourceCode/pages/diseases/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+import NavBar from "../../components/NavBar";
+import styles from "../../styles/Disease.module.scss";
+
+export async function getStaticProps() {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { diseases: data }
+  }
+}
+
+export default function Diseases( {diseases} ) {
+  
+  return (
+    <>
+      <Head>
+        <title>All Diseases</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <div className={styles.content}>
+        <h1>All Diseases</h1>
+        {diseases.map(disease => (
+          <div key={disease.id}>
+            <a className={styles.single}>
+              <h3>{ disease.name }</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
