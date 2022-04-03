2c5784b95a14d4a35f1f6009f5490f903e9d9035
alexandermbrown <alexbr5@bigpond.com>
2022-04-03 19:01:12 +1000

    Add report count to disease list

--- PHASE_2/Application_SourceCode/pages/diseases/index.js
+++ PHASE_2/Application_SourceCode/pages/diseases/index.js
@@ -44,7 +44,8 @@ export default function Diseases({ diseases }) {
               {disease.symptoms.length > 0 &&
                 <i>symptoms include {disease.symptoms.join(", ")}.</i>
               }
-                  
+              <div className="float_right" style={{fontSize: "0.9em"}}>{disease.report_count} reports</div>
+              <div style={{clear: "both"}}></div>
             </a>
           </Link>
         ))}
