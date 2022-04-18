67bed865ccaf147a89175d72756c92de6127d21a
alexandermbrown <alexbr5@bigpond.com>
2022-04-18 17:21:16 +1000

    Increase icon size

--- PHASE_2/Application_SourceCode/components/ReportHelp.js
+++ PHASE_2/Application_SourceCode/components/ReportHelp.js
@@ -6,7 +6,7 @@ import { IconContext } from "react-icons";
 function ReportHelp() {
   return (
   <div className={styles.reportHelp}>
-      <IconContext.Provider value={{size: "1.4em"}}>
+      <IconContext.Provider value={{size: "1.5em"}}>
         <MdInfoOutline />
       </IconContext.Provider>
       <span>the following articles were found which document diseases in this location</span>
