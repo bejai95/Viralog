3a70d83c23bfd546108059b37ae69ccf312f1074
alexandermbrown <alexbr5@bigpond.com>
2022-04-23 20:46:01 +1000

    Fix logarithmic default value

--- PHASE_2/Application_SourceCode/components/HighRiskDiseasesGraph.js
+++ PHASE_2/Application_SourceCode/components/HighRiskDiseasesGraph.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import { BarElement, LogarithmicScale, Chart as ChartJS, LinearScale } from 'chart.js';
-import {Bar} from 'react-chartjs-2';
+import { BarElement, LogarithmicScale, Chart as ChartJS, LinearScale } from "chart.js";
+import {Bar} from "react-chartjs-2";
 import styles from "../styles/HighRiskDiseaseGraph.module.scss";
 
 ChartJS.register(
@@ -19,22 +19,22 @@ export default function HighRiskDiseasesGraph( { xValues, yValues } ) {
       label: "Number of reports in the past 90 days",
       data: yValues,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)"
       ],
       borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)"
       ],
       borderWidth: 1
     }]
@@ -67,7 +67,7 @@ export default function HighRiskDiseasesGraph( { xValues, yValues } ) {
     <div>
       <Bar data={data} options={options} />
       <i>Graph scale type  </i> {" "}
-      <select className={styles.dropdown} onChange={changeScale} defaultValue="logarithmic">
+      <select className={styles.dropdown} onChange={changeScale} defaultValue={"logarithmic"}>
         <option value="linear">Linear Scale</option>
         <option value="logarithmic">Logarithmic Scale</option>
       </select>
