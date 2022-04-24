b2093359a9f7f486c754fa6c9bbfdcbca9c5a898
alexandermbrown <alexbr5@bigpond.com>
2022-04-24 13:53:31 +1000

    Add progress bar

--- PHASE_2/Application_SourceCode/pages/_app.js
+++ PHASE_2/Application_SourceCode/pages/_app.js
@@ -2,9 +2,16 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+
+Router.events.on("routeChangeStart",    () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError",    () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
